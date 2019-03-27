b5e07a4f237da2c9fc1a9b20d2df8a0bcdaf1616
Salvatore Torsello <salvatore.torsello08@gmail.com>
2019-03-27 20:31:42 +0100

    progetto base

---
+++ MDK-ARM/GCU_Model_debugTest/clutchmotor_init_wrapper.d
@@ -0,0 +1,3 @@
+gcu_model_debugtest\clutchmotor_init_wrapper.o: ..\Src\ClutchMotor_init_wrapper.c
+gcu_model_debugtest\clutchmotor_init_wrapper.o: ../Inc/rtwtypes.h
+gcu_model_debugtest\clutchmotor_init_wrapper.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
---
+++ MDK-ARM/GCU_Model_debugTest/efi_setcut_wrapper.d
@@ -0,0 +1,3 @@
+gcu_model_debugtest\efi_setcut_wrapper.o: ..\Src\Efi_setCut_wrapper.c
+gcu_model_debugtest\efi_setcut_wrapper.o: ../Inc/rtwtypes.h
+gcu_model_debugtest\efi_setcut_wrapper.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
