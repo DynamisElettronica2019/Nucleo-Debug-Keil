3aac99501fb06bf75b8bbe759ff68a9b84268753
SimoneSpecchia <simone.specchia26@gmail.com>
2019-03-29 18:55:11 +0100

    Ordinato i file nella repository

---
+++ GCU_Model_debug/MDK-ARM/GCU_Model_debugTest/packcanuart_wrapper.d
@@ -0,0 +1,3 @@
+gcu_model_debugtest\packcanuart_wrapper.o: ..\Src\PackCanUART_wrapper.c
+gcu_model_debugtest\packcanuart_wrapper.o: ../Inc/rtwtypes.h
+gcu_model_debugtest\packcanuart_wrapper.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
---
+++ GCU_Model_debug/MDK-ARM/RTE/_GCU_Model_debugTest/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GCU_Model_debugTest' 
+ * Target:  'GCU_Model_debugTest' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f7xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
