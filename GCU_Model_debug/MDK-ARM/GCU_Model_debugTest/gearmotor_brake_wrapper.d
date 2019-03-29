163280cb944236278b03b13af68cc62412e57028
SimoneSpecchia <simone.specchia26@gmail.com>
2019-03-29 23:29:01 +0100

    rimosso ogni file

--- GCU_Model_debug/MDK-ARM/GCU_Model_debugTest/gearmotor_brake_wrapper.d
+++
@@ -1,3 +0,0 @@
-gcu_model_debugtest\gearmotor_brake_wrapper.o: ..\Src\GearMotor_brake_wrapper.c
-gcu_model_debugtest\gearmotor_brake_wrapper.o: ../Inc/rtwtypes.h
-gcu_model_debugtest\gearmotor_brake_wrapper.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
--- GCU_Model_debug/MDK-ARM/GCU_Model_debugTest/gearmotor_turnleft_wrapper.d
+++
@@ -1,3 +0,0 @@
-gcu_model_debugtest\gearmotor_turnleft_wrapper.o: ..\Src\GearMotor_turnLeft_wrapper.c
-gcu_model_debugtest\gearmotor_turnleft_wrapper.o: ../Inc/rtwtypes.h
-gcu_model_debugtest\gearmotor_turnleft_wrapper.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
