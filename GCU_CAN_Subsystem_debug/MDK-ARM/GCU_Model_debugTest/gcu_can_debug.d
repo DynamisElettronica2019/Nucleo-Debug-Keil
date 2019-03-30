a0bb85dde794682e2bc0f56ea6b40b2c7047297b
SimoneSpecchia <simone.specchia26@gmail.com>
2019-03-30 13:14:33 +0100

    Aggiunta progetto CAN_ debug

---
+++ GCU_CAN_Subsystem_debug/MDK-ARM/GCU_Model_debugTest/gcu_can_debug.d
@@ -0,0 +1,3 @@
+gcu_model_debugtest\gcu_can_debug.o: ..\Src\GCU_CAN_debug.c
+gcu_model_debugtest\gcu_can_debug.o: ../Inc/GCU_CAN_debug.h
+gcu_model_debugtest\gcu_can_debug.o: ../Inc/rtwtypes.h
---
+++ GCU_Model_debug/MDK-ARM/GCU_Model_debugTest/enginecontrol_start_wrapper.d
@@ -0,0 +1,3 @@
+gcu_model_debugtest\enginecontrol_start_wrapper.o: ..\Src\EngineControl_start_wrapper.c
+gcu_model_debugtest\enginecontrol_start_wrapper.o: ../Inc/rtwtypes.h
+gcu_model_debugtest\enginecontrol_start_wrapper.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
