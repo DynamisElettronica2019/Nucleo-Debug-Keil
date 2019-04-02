7c9f58a9f46eea28e8afc1a6e9de8966ceaab642
SimoneSpecchia <simone.specchia26@gmail.com>
2019-04-02 13:09:07 +0200

    Configurato PWM su mxCube GCU_Model

--- GCU_Model_debug/Src/main.c
+++ GCU_Model_debug/Src/main.c
@@ -47,6 +47,7 @@
 /* Private variables ---------------------------------------------------------*/
 
 TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim4;
 
 UART_HandleTypeDef huart3;
 DMA_HandleTypeDef hdma_usart3_tx;
@@ -63,6 +64,7 @@ static void MX_DMA_Init(void);
 static void MX_USART3_UART_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_GFXSIMULATOR_Init(void);
+static void MX_TIM4_Init(void);
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
 
@@ -72,8 +74,10 @@ static void MX_GFXSIMULATOR_Init(void);
 /* USER CODE BEGIN 0 */
 uint8_t tempMsg[24];
 uint8_t rxData[DIM], readData = 0;
-int flag = 0;
-char header[] = "hdr", temp[] = {0,0,0}; 
+int flag = 0, pwmValue = 0, step = 0, dutyCycle = 0;
+char header[] = "hdr", temp[] = {0,0,0};
+
+void user_pwm_setvalue(uint16_t value);
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
@@ -142,6 +146,7 @@ int main(void)
   MX_USART3_UART_Init();
   MX_TIM2_Init();
   MX_GFXSIMULATOR_Init();
+  MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim2);
 	HAL_UART_Receive_IT(&huart3, rxData, 1);
@@ -149,6 +154,8 @@ int main(void)
 	GCU_Model_genCode_initialize();	
 	rtU.SelectMode = 1;
 	GCU_Model_genCode_step2();
+	
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -159,18 +166,26 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	//HAL_UART_Transmit(&huart3, &(rtY.Outport), 1, 50);
-		//GCU_Model_genCode_step2();
-		//GCU_Model_genCode_step1();
-		/*if(flag)
-		{	
-			GCU_Model_genCode_step1();
-			flag = 0;
-		}	*/
+		if(dutyCycle <= 0) 
+			step = 10;
+		
+		if(dutyCycle >= 100) 
+			step = -10;
+		
+		pwmValue += step;
+		dutyCycle += step;
+		pwmValue = htim4.Init.Period*dutyCycle/100;
+		
+		user_pwm_setvalue(pwmValue);
+		
+		
+		
+	
 		if( rtU.SelectMode == 3)
 			GCU_Model_genCode_step2();
-		//HAL_UART_Transmit(&huart3, (uint8_t *) &rtY.debugValues[3], /*WIDTH*/1 , 10);
-	  HAL_Delay(50);
+		
+	  user_pwm_setvalue(pwmValue);
+		HAL_Delay(100);
   }
   /* USER CODE END 3 */
 }
@@ -270,9 +285,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 1000;
+  htim2.Init.Prescaler = 1000-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 107;
+  htim2.Init.Period = 108-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
@@ -293,6 +308,65 @@ static void MX_TIM2_Init(void)
   /* USER CODE BEGIN TIM2_Init 2 */
 
   /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief TIM4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM4_Init(void)
+{
+
+  /* USER CODE BEGIN TIM4_Init 0 */
+
+  /* USER CODE END TIM4_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM4_Init 1 */
+
+  /* USER CODE END TIM4_Init 1 */
+  htim4.Instance = TIM4;
+  htim4.Init.Prescaler = 1080-1;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.Period = 2000-1;
+  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM4_Init 2 */
+
+  /* USER CODE END TIM4_Init 2 */
+  HAL_TIM_MspPostInit(&htim4);
 
 }
 
@@ -415,6 +489,19 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	}
 	//HAL_GPIO_WritePin(GPIOB, BluePin_Pin, GPIO_PIN_RESET);
   __enable_irq();
+}
+
+
+void user_pwm_setvalue(uint16_t value)
+{
+	TIM_OC_InitTypeDef sConfigOC;
+  
+	sConfigOC.OCMode = TIM_OCMODE_PWM1;
+	sConfigOC.Pulse = value;
+	sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+	sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+	HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_4);
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);  
 }
 /* USER CODE END 4 */
 
