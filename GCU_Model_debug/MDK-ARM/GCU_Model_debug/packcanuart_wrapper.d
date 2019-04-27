134e57c90b82ab505b7bcdf5334f69272216bf58
SimoneSpecchia <simone.specchia26@gmail.com>
2019-04-27 11:21:59 +0200

    Modificato packCAN e unpackCAN wrappers

--- GCU_Model_debug/MDK-ARM/GCU_Model_debug/packcanuart_wrapper.d
+++ GCU_Model_debug/MDK-ARM/GCU_Model_debug/packcanuart_wrapper.d
@@ -1,3 +1,2 @@
 gcu_model_debug\packcanuart_wrapper.o: ..\Src\S-Function_Wrappers\PackCanUART_wrapper.c
 gcu_model_debug\packcanuart_wrapper.o: ../Inc/rtwtypes.h
-gcu_model_debug\packcanuart_wrapper.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
