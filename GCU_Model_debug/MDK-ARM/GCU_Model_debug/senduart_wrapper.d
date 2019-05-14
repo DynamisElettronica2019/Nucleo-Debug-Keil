1515285715b6151ab0dbd66daa245c0c0e187121
SimoneSpecchia <simone.specchia26@gmail.com>
2019-05-14 22:24:12 +0200

    Modificato include di alcune s-functions

--- GCU_Model_debug/MDK-ARM/GCU_Model_debug/senduart_wrapper.d
+++ GCU_Model_debug/MDK-ARM/GCU_Model_debug/senduart_wrapper.d
@@ -1,5 +1,6 @@
 gcu_model_debug\senduart_wrapper.o: ..\Src\S-Function_Wrappers\sendUART_wrapper.c
 gcu_model_debug\senduart_wrapper.o: ../Inc/rtwtypes.h
+gcu_model_debug\senduart_wrapper.o: ../Inc/constant_defines.h
 gcu_model_debug\senduart_wrapper.o: ../Inc/pin_defines.h
 gcu_model_debug\senduart_wrapper.o: ../Inc/main.h
 gcu_model_debug\senduart_wrapper.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h
@@ -38,4 +39,3 @@ gcu_model_debug\senduart_wrapper.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7x
 gcu_model_debug\senduart_wrapper.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_tim_ex.h
 gcu_model_debug\senduart_wrapper.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h
 gcu_model_debug\senduart_wrapper.o: ../Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h
-gcu_model_debug\senduart_wrapper.o: ../Inc/constant_defines.h
